02ea0d1678cfac97e0a7ab48dac493477b027273
牛辉 <niuhui@niuhuideMacBook-Pro.local>
2017-03-13 23:05:03 +0800

    创建工程

---
+++ Mac-YunIM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mac-YunIM
+//
+//  Created by 牛辉 on 2017/3/13.
+//  Copyright © 2017年 Niu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
