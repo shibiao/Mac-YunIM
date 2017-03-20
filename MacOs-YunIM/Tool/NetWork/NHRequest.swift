281b880d76b90bbbc01a27311108d99f300a2f0a
牛辉 <niuhui@niuhuideMacBook-Pro.local>
2017-03-20 13:40:14 +0800

    网络请求 get post

---
+++ MacOs-YunIM/Tool/NetWork/NHRequest.swift
@@ -0,0 +1,46 @@
+//
+//  NHRequest.swift
+//  MacOs-YunIM
+//
+//  Created by 牛辉 on 2017/3/20.
+//  Copyright © 2017年 Niu. All rights reserved.
+//
+
+import Cocoa
+import Alamofire
+enum Response<T,U> {
+    case Success(T)
+    case Failure(U)
+}
+typealias callBack = (Response<Any, String>) -> Void
+
+struct NHRequest {
+    
+    static func request(_ url: String,request: @escaping callBack) {
+        Alamofire.request(url,headers:self.UserAgent).responseJSON { (response) in
+            switch response.result {
+            case .success(let data):
+                request(.Success(data))
+            case .failure(let error):
+                request(.Failure(error.localizedDescription))
+            }
+        }
+    }
+    static func request(_ url: String , parms: [String : Any]?,request: @escaping callBack) {
+        Alamofire.request(url,parameters:parms,headers:self.UserAgent).responseJSON { (response) in
+            switch response.result {
+            case .success(let data):
+                request(.Success(data)) 
+            case .failure(let error):
+                request(.Failure(error.localizedDescription))
+            }
+        }
+    }
+    static var UserAgent : [String:String] {
+        let version = ProcessInfo.processInfo.operatingSystemVersionString
+        let str = "i/\(Bundle.main.infoDictionary!["CFBundleShortVersionString"])\(version)/iphone/wifi"
+        return ["User-Agent" : str]
+    }
+    
+    
+}
