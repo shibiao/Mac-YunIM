c5e23208d41162f5bcf1dc97b3b185780a78f4a5
牛辉 <niuhui_sunny@sina.com>
2017-04-02 23:35:34 +0800

    登陆的业务模块

---
+++ MacOs-YunIM/IMTool/IMBusiness/Login/IMloginProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  IMloginProtocol.swift
+//  MacOs-YunIM
+//
+//  Created by 牛辉 on 2017/4/2.
+//  Copyright © 2017年 Niu. All rights reserved.
+//
+
+import Cocoa
+
+class IMloginProtocol: NSObject {
+
+}
